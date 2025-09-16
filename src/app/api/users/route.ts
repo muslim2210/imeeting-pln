3516439d4b1c6d40061e3a5ce8bdd29264b0a902
Bukhori Muslim <bukhori.muslim2210@gmail.com>
2025-09-16 14:05:26 +0700

    (fix) : fix deploy invalid user

--- src/app/api/users/route.ts
+++ src/app/api/users/route.ts
@@ -5,7 +5,7 @@ const prisma = new PrismaClient();
 
 export async function GET() {
   try {
-    const users = await prisma.User.findMany({
+    const users = await prisma.user.findMany({
       orderBy: { name: "asc" },
     });
     return NextResponse.json({ success: true, data: users });
